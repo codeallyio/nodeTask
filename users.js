0676c4d7a6f5415239ba55b8e053d0f8b6788bb0
Adam Jędrzejec <adamjedrzejec@gmail.com>
2020-10-15 15:19:09 +0200

    [add] example solution -> for testing purposes

--- users.js
+++ users.js
@@ -1,20 +1,86 @@
 const { Router } = require('express')
 const { generateAge, generateGender, generateEmail } = require('./dataGenerator')
+const { validateGender, validateAge, validateEmail } = require('./dataValidator')
 
 
 const router = Router()
-const usersPseudoDB = []
+let usersPseudoDB = []
 
+let serialID = 1
 
-router.get('/getUsers', () => { })
+router.get('/getUsers', (req, res) => {
+    res.status(200).send(usersPseudoDB)
+})
 
-router.get('/getUser', () => { }) // by ID
+router.get('/getUser/:id', (req, res) => {
+    const { id } = req.params
 
-router.post('/generateUser', () => { })
+    const user = usersPseudoDB.find(u => u.id == id)
 
-router.put('/updateUser', () => { }) // by ID
+    if (!user) {
+        res.status(404).send({ message: `User with id ${id} not found` })
+    }
 
-router.delete('/deleteUser', () => { }) // by ID
+    res.status(200).send({ message: 'User found successfully', user })
+})
+
+router.post('/generateUser', (req, res) => {
+    const urlParams = req.query
+
+    const age = generateAge(urlParams.age)
+    const gender = generateGender(urlParams.gender)
+    const email = generateEmail(urlParams.email, gender)
+
+    id = serialID++
+
+    const user = { id, age, gender, email }
+
+    usersPseudoDB.push(user)
+
+    res.status(201).send({ message: 'User successfully created', user })
+})
+
+router.put('/updateUser/:id', (req, res) => {
+    const { id } = req.params
+
+    const user = usersPseudoDB.find(u => u.id == id)
+
+    if (!user) {
+        res.status(404).send({ message: `User with id ${id} not found` })
+    }
+
+    const updates = {}
+
+    if (req.body != undefined) {
+        if (validateAge(req.body.age)) {
+            updates.age = req.body.age
+        }
+        if (validateGender(req.body.gender)) {
+            updates.gender = req.body.gender
+        }
+        if (validateEmail(req.body.email)) {
+            updates.email = req.body.email
+        }
+    }
+
+    Object.assign(user, updates)
+
+    res.status(200).send(user)
+})
+
+router.delete('/deleteUser/:id', (req, res) => {
+    const { id } = req.params
+
+    const user = usersPseudoDB.find(u => u.id == id)
+
+    if (!user) {
+        res.status(404).send({ message: `User with id ${id} not found` })
+    }
+
+    usersPseudoDB = usersPseudoDB.filter(u => u.id != id)
+
+    res.status(404).send({ message: 'User deleted successfully', user })
+})
 
 
 module.exports = router
