b5be74e4a20c7e0755a69119be1e2823af732ba4
Adam Jędrzejec <adamjedrzejec@gmail.com>
2020-10-21 17:51:42 +0200

    [fix] solution try-catch blocks when number id is passed + small fixes

--- users.js
+++ users.js
@@ -4,22 +4,20 @@ const { validateGender, validateAge, validateEmail } = require('./dataValidator'
 
 const userModel = require('./user-model')
 
-var db = require('./usersPseudoDB')
-
 
 const router = Router()
 
 router.get('/users', async (req, res) => { // get all users
     const users = await userModel.find()
 
-    res.status(200).send({ users })
+    res.status(200).send(users)
 })
 
 router.get('/user/:id', (req, res) => { // get single user
     const { id } = req.params
 
     userModel.findById(id, (err, user) => {
-        if (err) {
+        if (err || user === null) {
             return res.status(404).send({ message: `User with id ${id} not found` })
         }
         res.status(200).send({ message: 'User found successfully', user })
@@ -61,32 +59,44 @@ router.put('/user/:id', async (req, res) => { // update user
         }
     }
 
-    const updatedUser = await userModel.updateOne({
-        "_id": id
-    }, updates)
+    try {
+        const updatedUser = await userModel.updateOne({
+            "_id": id
+        }, updates)
 
-    if (!updatedUser.n) {
+        if (!updatedUser.n) {
+            return res.status(404).send({ message: `User with id ${id} not found` })
+        }
+
+        // console.log(updatedUser)
+
+        userModel.findById(id, (err, user) => {
+            res.status(200).send({ message: 'User updated successfully', user })
+        })
+    } catch {
         return res.status(404).send({ message: `User with id ${id} not found` })
     }
-
-    userModel.findById(id, (err, user) => {
-        res.status(200).send({ message: 'User updated successfully', user })
-    })
 })
 
 router.delete('/user/:id', async (req, res) => { // delete user
     const { id } = req.params
 
 
-    const deletedUser = await userModel.findOneAndDelete({
-        "_id": id,
-    })
+    try {
+        const deletedUser = await userModel.findOneAndDelete({
+            "_id": id,
+        })
 
-    if (deletedUser === null) {
+        if (deletedUser === null) {
+            throw Error('user not found')
+            // return res.status(404).send({ message: `User with id ${id} not found` })
+        }
+
+        res.status(200).send({ message: 'User deleted successfully', user: deletedUser })
+    } catch {
         return res.status(404).send({ message: `User with id ${id} not found` })
-    }
 
-    res.status(200).send({ message: 'User deleted successfully', user: deletedUser })
+    }
 })
 
 
