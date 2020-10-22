8e63bc8873c6fe69b95141991ff3c7deecc159aa
Adam Jędrzejec <adamjedrzejec@gmail.com>
2020-10-22 21:03:48 +0200

    [add] mongo-init.sh descriptions

--- mongo-init.sh
+++ mongo-init.sh
@@ -1,3 +1,6 @@
+# `sudo mongod` default location is /data/db
+# we have to be sure it exists
+
 if [ ! -d "/data" ] 
 then
     sudo mkdir /data
@@ -7,5 +10,8 @@ if [ ! -d "/data/db" ]
 then
     sudo mkdir /data/db
 fi
+
+# run `mongod` as admin, to get access to /data/db
+# `--fork --logpath /data/db/mongod.log` to run in background
 
 sudo mongod --fork --logpath /data/db/mongod.log
