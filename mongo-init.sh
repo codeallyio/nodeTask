d57cae1e9586a8aee1f44922ebf10fca2954ade7
Adam Jędrzejec <adamjedrzejec@gmail.com>
2020-10-23 17:26:46 +0200

    [fix] mongo-init script

--- mongo-init.sh
+++ mongo-init.sh
@@ -11,7 +11,15 @@ then
     sudo mkdir /data/db
 fi
 
+if ! sudo ss -tulpn | grep ':27017'
+then
+    echo "Starting MongoDB..."
 # run `mongod` as admin, to get access to /data/db
 # `--fork --logpath /data/db/mongod.log` to run in background
+    sudo mongod --fork --logpath /data/db/mongod.log
+fi
 
-sudo mongod --fork --logpath /data/db/mongod.log
+if sudo ss -tulpn | grep ':27017'
+then
+    echo "MongoDB is already up"
+fi
